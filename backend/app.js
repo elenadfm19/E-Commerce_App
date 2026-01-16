f63ec6f19e76eb239b720297976d06160d081b40
elenadfm19 <elenadefrias@gmail.com>
2026-01-16 17:10:31 +0000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 // `origin` specifies which frontend URL is allowed to make requests.
 // `credentials: true` allows session cookies to be sent with cross-origin requests.
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 // Creates a session by using the express-session middleware for persistent login sessions
 // Required for Passport's login session management
