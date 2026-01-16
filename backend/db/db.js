9298d1ee41a5e335520085882ea5795f3683a634
elenadfm19 <elenadefrias@gmail.com>
2026-01-16 18:00:31 +0000

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -12,6 +12,9 @@ const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME, // <-- now DB exists
+    ssl: {
+    rejectUnauthorized: false, // required for Render Postgres
+  },
 });
 
 module.exports = db;
